741fa8ffa62421c95e36eac550b0b763cb716513
David Gonzalez Martin <davidgm94.work@protonmail.com>
2021-10-22 13:30:28 -0500

    Got to kernel_main

--- src/kernel/main.zig
+++ src/kernel/main.zig
@@ -1,15 +1,8 @@
 const x86 = @import("x86.zig");
-pub export fn interrupt_handler() callconv(.C) noreturn
-{
-    while (true) { }
-}
+// @INFO: we need this for the symbols to be exported
+comptime { _ = x86.interrupt_handler; _ = x86.syscall; }
 
-pub export fn syscall() callconv(.C) noreturn
-{
-    while (true) { }
-}
-
-export fn kernel_main() noreturn
+export fn kernel_main() callconv(.C) noreturn
 {
     while (true) { }
 }
