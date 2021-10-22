9a86da8f1a808d8cc8e37a55068850c5f611fd1c
David Gonzalez Martin <davidgm94.work@protonmail.com>
2021-10-22 11:35:01 -0500

    Enable syscall extensions

--- src/kernel/x86.zig
+++ src/kernel/x86.zig
@@ -1,15 +1,9 @@
-const x86 = @import("x86.zig");
 pub export fn interrupt_handler() callconv(.C) noreturn
 {
     while (true) { }
 }
 
 pub export fn syscall() callconv(.C) noreturn
-{
-    while (true) { }
-}
-
-export fn kernel_main() noreturn
 {
     while (true) { }
 }
