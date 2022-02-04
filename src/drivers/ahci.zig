69b00abba651501ca631a849780b8c14755d392b
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-02-03 18:32:28 -0600

    More cleaning

--- src/drivers/ahci.zig
+++
@@ -1,9 +0,0 @@
-pub const Driver = struct
-{
-    pub fn init(self: *@This()) void
-    {
-        _ = self;
-    }
-};
-
-pub var driver: Driver = undefined;
--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const Kernel = @import("kernel.zig");
-pub fn panic(message: []const u8, stack_trace: ?*std.builtin.StackTrace) noreturn
-{
-    _ = stack_trace;
-    // @TODO: this is a workaround to get message debug information
-    Kernel.panic_raw(message);
-}
